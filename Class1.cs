c2b7d042ef133eefc8ca4002611a9bf971cfcb7f
JoseAguirre <jose_aguirre@live.com.mx>
2022-02-11 16:48:42 -0600

    Exercise number 2 of unosquare training

---
+++ Class1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace Exercise2
+{
+    internal class Class1
+    {
+        private int idEmployee;
+        private string firstName;
+        private string lastName;
+        private int age;
+        private string job;
+        private string company;
+
+
+
+        /*Default Constructor*/
+        public Class1()
+        {
+
+        }
+
+
+        /*Constructor for unemployed */
+        public Class1(int idEmployee,string firstName, string lastName, int age)
+        {
+            this.idEmployee = idEmployee;
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.age = age;
+
+        }
+        /*Constructur for worker*/
+        public Class1(int idEmployee, string firstName, string lastName, int age, string job, string company){
+            this.idEmployee = idEmployee;
+            this.firstName = firstName;
+            this.lastName = lastName;   
+            this.age = age;
+            this.job = job;
+            this.company = company;
+
+        }
+
+
+        public string updateEmployee(string firstName, string lastName, int age, string job, string company)
+        {
+            return "Employee information:\nFull Name:" + firstName + " " + lastName + "\nAge: " + age + "\nJob: " + job + "\nCompany: " + company;
+        }
+        
+            public string updateEmployee(string job, string company)
+        {
+            this.job = job;
+            this.company = company;
+            return "Employee information: " +"\nId Employee: "+this.idEmployee+"\nFull Name:" + this.firstName + " " + this.lastName + "\nAge: " + this.age + "\nJob: " + this.job + "\nCompany: " + this.company;
+        }
+        public string getEmployee()
+        {
+            return "Employee information: " + "\nId Employee: " + this.idEmployee + "\nFull Name:" + this.firstName + " " + this.lastName + "\nAge: " + this.age + "\nJob: " + this.job + "\nCompany: " + this.company;
+        }
+
+
+        public static void printNumbersInDiagonal(int limit)
+        {
+            for (int i = 1; i <= limit;i++) {
+                Console.WriteLine(i.ToString().PadLeft(i));
+             }
+        }
+
+        public static bool isPalidrome(string word)
+        {
+            /*Remove every special character like '!. */
+            string wordFormatted = Regex.Replace(word.Normalize(NormalizationForm.FormD), @"[^a-zA-z0-9 ]+", "");
+
+            /*Remove blank spaces and make an upper case*/
+            char[] chars = wordFormatted.ToUpper().Replace(" ","").ToCharArray();
+            for(int x = 1; x < chars.Length; x++)
+            {
+                char a = chars[x - 1];
+                char b = chars[chars.Length - x];
+                if (chars[x - 1] != chars[chars.Length - x])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 employee = new Class1(1, "Jose", "Aguirre", 31);
+            Class1 employee2 = new Class1(2, "Dario", "Gamboa", 31, "SDET", "UnoSquare");
+
+
+            Console.WriteLine("-----------------------------------------------------");
+            Console.WriteLine("------------Apply constructor overloading.-----------");
+            Console.WriteLine("-----------------------------------------------------");
+            Console.WriteLine(employee.getEmployee());
+            Console.WriteLine("---------------------------------------");
+            Console.WriteLine(employee.updateEmployee("Tester", "UnoSquare"));
+            Console.WriteLine("---------------------------------------");
+            Console.WriteLine(employee2.getEmployee());
+
+        
+            Console.WriteLine("---------------------------------------------------------------------------------");
+            Console.WriteLine("Create a method to verify if a string is a palindrome (Use the created property).");
+            
+            Console.WriteLine("---------------------------------------------------------------------------------");
+            Console.WriteLine("Alí tomó tila. --->" + Class1.isPalidrome("Alí tomó tila."));
+            Console.WriteLine("Ají traga la lagartija. ---->" + Class1.isPalidrome("Ají traga la lagartija."));
+            Console.WriteLine("Lo sé, dama de sol. --->" + Class1.isPalidrome("Lo sé, dama de sol."));
+            Console.WriteLine("Esto no es un palindromo --->" + Class1.isPalidrome("Esto no es un palindromo"));
+
+            Console.WriteLine("-----------------------------------------------------");
+            Console.WriteLine("----Create a method to print numbers in diagonal.----");
+            Console.WriteLine("-----------------------------------------------------");
+            Class1.printNumbersInDiagonal(100);
+
+
+
+
+
+            Console.ReadKey();
+
+            
+        }
+    }
+}
